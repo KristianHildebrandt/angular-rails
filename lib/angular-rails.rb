207399e16060115f6f72dc1ac81571eb191fbff9
Kristian Hildebrandt <hildebrandt.kristian@gmail.com>
2013-05-15 23:13:57 +0200

    version bump, fuck tabs n spaces

--- lib/angular-rails.rb
+++ lib/angular-rails.rb
@@ -1,6 +1,6 @@
 require "rails"
 
 module AngularRails
-	class Engine < Rails::Engine
-	end
+  class Engine < Rails::Engine
+  end
 end
--- lib/angular-rails/version.rb
+++ lib/angular-rails/version.rb
@@ -1,3 +1,3 @@
 module AngularRails
-  VERSION = "0.0.12"
+  VERSION = "0.0.13"
 end
